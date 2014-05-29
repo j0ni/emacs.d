967b9bfcfcc49493001d4a0a3443b516bc226edd
J Irving <jonathan.irving@turn.com>
2014-05-29 00:20:28 -0400

    Add Sam Aaron's modifications for eval-sexp-fu

--- lisp/j0ni-lisp.el
+++ lisp/j0ni-lisp.el
@@ -18,6 +18,7 @@
 ;; Make em light up
 (require 'highlight)
 (packages-require '(eval-sexp-fu hl-sexp))
+(require 'eval-sexp-fu)
 (add-lisp-hook 'hl-sexp-mode)
 
 
@@ -104,8 +105,110 @@
 (add-hook 'cider-mode-hook (lambda ()
                              (cider-turn-on-eldoc-mode)
                              (diminish 'eldoc-mode)
-                             (diminish 'cider-mode)))
+                             (diminish 'cider-mode)
+                             (live-esf-initialize-cider)))
 
+;; Stolen from Sam Aaron to get eval-sexp-fu working with clojure
+(defun live-bounds-of-preceding-sexp ()
+  "Return the bounds of sexp before the point. Copies semantics
+   directly from the fn preceding-sexp to ensure highlighted area
+   is identical to that which is evaluated."
+  (let ((opoint (point))
+        ignore-quotes
+        expr)
+    (save-excursion
+      (with-syntax-table emacs-lisp-mode-syntax-table
+        ;; If this sexp appears to be enclosed in `...'
+        ;; then ignore the surrounding quotes.
+        (setq ignore-quotes
+              (or (eq (following-char) ?\')
+                  (eq (preceding-char) ?\')))
+        (forward-sexp -1)
+        ;; If we were after `?\e' (or similar case),
+        ;; use the whole thing, not just the `e'.
+        (when (eq (preceding-char) ?\\)
+          (forward-char -1)
+          (when (eq (preceding-char) ??)
+            (forward-char -1)))
+
+        ;; Skip over hash table read syntax.
+        (and (> (point) (1+ (point-min)))
+             (looking-back "#s" (- (point) 2))
+             (forward-char -2))
+
+        ;; Skip over `#N='s.
+        (when (eq (preceding-char) ?=)
+          (let (labeled-p)
+            (save-excursion
+              (skip-chars-backward "0-9#=")
+              (setq labeled-p (looking-at "\\(#[0-9]+=\\)+")))
+            (when labeled-p
+              (forward-sexp -1))))
+
+        (save-restriction
+          ;; vladimir@cs.ualberta.ca 30-Jul-1997: skip ` in
+          ;; `variable' so that the value is returned, not the
+          ;; name
+          (if (and ignore-quotes
+                   (eq (following-char) ?`))
+              (forward-char))
+          (cons (point) opoint))))))
+
+(defun live-bounds-of-defun ()
+  "Return the bounds of the defun around point. Copies semantics
+   directly from the fn eval-defun-2 to ensure highlighted area
+   is identical to that which is evaluated."
+  (save-excursion
+    (end-of-defun)
+    (beginning-of-defun)
+    (setq beg (point))
+    (read (current-buffer))
+    (setq end (point))
+    (cons beg end)))
+
+;; fix up esf to highlight exactly what emacs evaluates
+(defun live-esf-initialize-elisp ()
+  (define-eval-sexp-fu-flash-command eval-last-sexp
+    (eval-sexp-fu-flash (when (ignore-errors (preceding-sexp))
+                          (with-esf-end-of-sexp
+                            (live-bounds-of-preceding-sexp)))))
+  (define-eval-sexp-fu-flash-command eval-defun
+    (eval-sexp-fu-flash (live-bounds-of-defun))))
+
+(live-esf-initialize-elisp)
+
+;; cider extensions
+
+
+(defun live-bounds-of-cider-last-sexp ()
+  "Return the bounds of the defun around point. Copies semantics
+   directly from the fn cider-last-sexp to ensure highlighted
+   area is identical to that which is evaluated."
+  (cons (save-excursion (backward-sexp) (point)) (point)))
+
+(defun live-esf-initialize-cider ()
+  (define-eval-sexp-fu-flash-command cider-eval-last-sexp
+    (eval-sexp-fu-flash (live-bounds-of-cider-last-sexp)))
+
+  (define-eval-sexp-fu-flash-command cider-pprint-eval-last-sexp
+    (eval-sexp-fu-flash (live-bounds-of-cider-last-sexp)))
+
+  (define-eval-sexp-fu-flash-command cider-eval-defun-at-point
+    (eval-sexp-fu-flash (let ((bounds (cider--region-for-defun-at-point)))
+                          (cons (first bounds) (second bounds)))))
+
+
+  (progn
+    ;; Defines:
+    ;; `eval-sexp-fu-cider-sexp-inner-list',
+    ;; `eval-sexp-fu-cider-sexp-inner-sexp'
+    ;; and the pprint variants respectively.
+    (define-eval-sexp-fu-eval-sexp eval-sexp-fu-cider-eval-sexp
+      cider-eval-last-sexp)
+    (define-eval-sexp-fu-eval-sexp eval-sexp-fu-cider-pprint-eval-sexp
+      cider-pprint-eval-last-sexp)))
+
+;; end of Sam's code
 
 ;; Run tests in nRepl
 (defun nrepl-run-tests (ns)
@@ -116,6 +219,7 @@
     (nrepl-send-string
      (format "(clojure.test/run-tests '%s)" ns)
      nrepl-buffer-ns (nrepl-handler (current-buffer)))))
+
 (eval-after-load "clojure-mode"
   '(define-key clojure-mode-map (kbd "C-c C-,") 'nrepl-run-tests))
 
@@ -178,7 +282,6 @@ Display the results in a hyperlinked *compilation* buffer."
     (read-kbd-macro paredit-backward-delete-key) nil))
 
 (add-hook 'slime-repl-mode-hook 'slime-repl-mode-custom)
-
 
 
 (provide 'j0ni-lisp)
