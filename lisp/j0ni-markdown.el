879e644dfbb550af50c596d36357d09d6e30e363
J Irving <j@lollyshouse.ca>
2018-11-23 09:15:45 -0500

    Better markdown rendering

--- lisp/j0ni-markdown.el
+++ lisp/j0ni-markdown.el
@@ -3,7 +3,7 @@
 (use-package markdown-mode
   :ensure t
   :init
-  (setq-default markdown-command "pandoc -S -s --self-contained -f markdown -t html5 ")
+  (setq-default markdown-command "pandoc -s --self-contained -f markdown+smart -t html5 ")
   (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
   (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
   (add-to-list 'auto-mode-alist '("\\.mdown\\'" . markdown-mode)))
