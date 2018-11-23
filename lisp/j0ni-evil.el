bf4b203393064e397fa01cbdd49e21ac720d3c9c
J Irving <j@lollyshouse.ca>
2018-11-23 08:42:39 -0500

    Improve evil-mode setup, but still don't use it

--- lisp/j0ni-evil.el
+++ lisp/j0ni-evil.el
@@ -1,5 +1,17 @@
 ;;; j0ni-evil.el -- Evil-mode setup
 
+;; (use-package evil-cleverparens
+;;   :ensure t
+;;   :commands evil-cleverparens-mode
+;;   :init
+;;   (setq evil-move-beyond-eol t)
+;;   :hook ((paredit-mode . evil-cleverparens-mode)
+;;          (lispy-mode . evil-cleverparens-mode)))
+
 (use-package evil)
+
+(use-package evil-smartparens
+  :after (:all smartparens evil)
+  :config (add-hook 'smartparens-enabled-hook #'evil-smartparens-mode))
 
 (provide 'j0ni-evil)
