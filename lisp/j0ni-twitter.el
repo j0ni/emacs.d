0c2d91f8f951eb5b87bb37d0542eb0a44687e4e7
J Irving <j@lollyshouse.ca>
2020-01-22 12:49:35 -0500

    Some modernization

--- lisp/j0ni-twitter.el
+++ lisp/j0ni-twitter.el
@@ -1,18 +1,19 @@
 ;;; j0ni-twitter.el --- twitter setup
 
-(package-require 'twittering-mode)
+(use-package twittering-mode
+  :init
 
-(setq twittering-use-master-password t)
+  (setq twittering-use-master-password t)
 
-(setq twittering-icon-mode nil)         ; Show icons
+  (setq twittering-icon-mode nil)         ; Show icons
 
-(setq twittering-timer-interval 300)    ; Update your timeline each 300
+  (setq twittering-timer-interval 300)    ; Update your timeline each 300
                                         ; seconds (5 minutes)
 
-(setq twittering-url-show-status nil)   ; Keeps the echo area from showing all
+  (setq twittering-url-show-status nil)   ; Keeps the echo area from showing all
                                         ; the http processes
 
-(setq twittering-status-format
-      "%RT{%FACE[bold]{RT}}%i %s/%S,  %@:\n%FOLD[  ]{%T // from %f%L%r%R%QT{\n+----\n%FOLD[|]{%i %s/%S,  %@:\n%FOLD[  ]{%T // from %f%L%r%R}}\n+----}}\n ")
+  (setq twittering-status-format
+        "%RT{%FACE[bold]{RT}}%i %s/%S,  %@:\n%FOLD[  ]{%T // from %f%L%r%R%QT{\n+----\n%FOLD[|]{%i %s/%S,  %@:\n%FOLD[  ]{%T // from %f%L%r%R}}\n+----}}\n "))
 
 (provide 'j0ni-twitter)
