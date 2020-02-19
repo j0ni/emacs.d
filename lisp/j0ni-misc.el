d8ddadb62bf3d218fbcda537a269e1d760e5b3b2
J Irving <j@lollyshouse.ca>
2020-02-18 21:58:02 -0500

    Remove unused bits and add back useful imenu stuff

--- lisp/j0ni-misc.el
+++ lisp/j0ni-misc.el
@@ -1,17 +1,17 @@
 ;;; j0ni-misc --- stuff that doesn't go anywhere else
 
 (use-package ssh-config-mode)
-(use-package paredit-everywhere)
+(use-package paredit-everywhere
+  :config (paredit-everywhere-mode 1))
+
 (use-package terraform-mode)
 (use-package csv-mode)
+
+(setq cache-long-scans t)
 
 (use-package browse-kill-ring
   :config
   (browse-kill-ring-default-keybindings))
-
-;; (use-package smart-tab
-;;   ;; :config (global-smart-tab-mode)
-;;   )
 
 ;; (use-package multiple-cursors
 ;;   :bind (("C-S-c C-S-c" . mc/edit-lines)
@@ -21,8 +21,6 @@
 
 ;; get the `r` key in the debugger for nice formatting and frame selection
 (require 'pp-debug)
-
-(use-package fish-mode)
 
 (use-package drag-stuff
   :config
@@ -37,13 +35,13 @@
   :hook
   ((sql-mode . sqlind-minor-mode)))
 
-;; (use-package imenu-list
-;;   :bind
-;;   ("C-," . imenu-list-smart-toggle)
+(use-package imenu-list
+  :bind
+  ("C-," . imenu-list-smart-toggle)
 
-;;   :init
-;;   (setq imenu-list-auto-resize t)
-;;   (setq imenu-list-focus-after-activation t))
+  :init
+  (setq imenu-list-auto-resize t)
+  (setq imenu-list-focus-after-activation t))
 
 (use-package pinentry
   :init
@@ -55,40 +53,11 @@
   :init
   (pinentry-start))
 
-;; (use-package dumb-jump
-;;   :bind (("M-g o" . dumb-jump-go-other-window)
-;;          ("M-g j" . dumb-jump-go)
-;;          ("M-g i" . dumb-jump-go-prompt)
-;;          ("M-g x" . dumb-jump-go-prefer-external)
-;;          ("M-g z" . dumb-jump-go-prefer-external-other-window)
-
-;;          ;; lets see how this goes
-;;          ("M-." . dumb-jump-go)
-;;          ("M-," . dumb-jump-back))
-
-;;   :init
-;;   (setq dumb-jump-selector 'ivy)
-;;   (setq dumb-jump-force-searcher 'ag)
-
-;;   :config (dumb-jump-mode))
-
-;; manage history better
-;; (add-hook 'after-init-hook 'session-initialize)
-
 ;; Let's see what we're running on
 (setq on-console (null window-system))
 
 ;; Don't defer screen updates when performing operations
 (setq redisplay-dont-pause t)
-
-;; find-file-in-project
-;; (use-package find-file-in-project
-;;   :init
-;;   (setq ffip-full-paths t))
-
-;; (use-package all-the-icons-dired
-;;   ;; M-x all-the-icons-install-fonts
-;;   :commands (all-the-icons-dired-mode))
 
 ;; Useful for figuring out complicated old code
 (use-package highlight-symbol
@@ -104,7 +73,6 @@
 
 ;; undo-tree
 (use-package undo-tree
-  :diminish nil
   :config
   (global-undo-tree-mode t))
 
@@ -119,10 +87,7 @@
         '(lambda ()
            (format " project[%s]" (projectile-project-name))))
   ;; set elsewhere
-  ;; (setq projectile-completion-system 'ivy)
-  (setq projectile-completion-system 'ido)
-
-  :diminish projectile-mode)
+  (setq projectile-completion-system 'ivy))
 
 (use-package projectile-ripgrep)
 
@@ -137,10 +102,6 @@
 ;; Make it easy
 (defun turn-off-auto-fill ()
   (auto-fill-mode -1))
-
-;; In general we only want this for comments - esk-local-comment-auto-fill
-;; should take care of this. But I'm not sure I want auto-fill at all tbh.
-;; (setq-default comment-auto-fill-only-comments t)
 
 (use-package smartparens
   :config
@@ -160,34 +121,11 @@
 ;; deal with broken find-file-at-point
 (setq ffap-machine-p-known 'reject)
 
-;; (use-package sublimity
-;;   :init
-;;   (setq sublimity-scroll-weight 5)
-;;   (setq sublimity-scroll-drift-length 10)
-;;   ;; (setq sublimity-map-size 20)
-;;   ;; (setq sublimity-map-fraction 0.3)
-;;   ;; (setq sublimity-map-text-scale -7)
-;;   :config
-;;   (require 'sublimity)
-;;   (require 'sublimity-scroll)
-;;   ;; (require 'sublimity-map)
-;;   ;; (require 'sublimity-attractive)
-;;   (sublimity-mode 1))
-
-;; nice scrolling
-;; (use-package smooth-scrolling
-;;   :init
-;;   (setq smooth-scroll-margin 2)
-
-;;   :config
-;;   (smooth-scrolling-mode))
-
 (setq scroll-step 0)
 (setq scroll-margin 2)
 (setq auto-window-vscroll nil)
 ;; be sure to set this to 0 in any auto-scrolling buffers
 (setq scroll-conservatively 100000)
-;; (setq scroll-conservatively 1)
 (setq scroll-preserve-screen-position t)
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ; one line at a time
 (setq mouse-wheel-progressive-speed t)              ; don't accelerate scrolling
@@ -215,8 +153,7 @@
 
 ;; which-key
 (use-package which-key
-  :config (which-key-mode 1)
-  :diminish nil)
+  :config (which-key-mode 1))
 
 ;; silver searcher
 (use-package ag
@@ -238,10 +175,6 @@
   :config (require 'kotlin-mode-lexer))
 (use-package gradle-mode
   :hook (kotlin-mode))
-
-;; puppet-mode sucks right now
-;; (use-package puppet-mode)
-;; (add-hook 'puppet-mode 'turn-on-smartparens-mode)
 
 ;; winner restores unintentionally closed windows
 (winner-mode t)
@@ -279,7 +212,5 @@
 
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 ;; (add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
-
-(use-package vterm)
 
 (provide 'j0ni-misc)
